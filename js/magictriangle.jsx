cd484891ea8bc361f1b133a53711e97613efdf0e
Lauren Tomasello <lt696@york.ac.uk>
2016-07-21 17:48:01 +0100

    Starting to add the generation of descriptors

--- js/magictriangle.jsx
+++ js/magictriangle.jsx
@@ -9,8 +9,20 @@ class MagicTriangle extends React.Component {
 	}
 
 	render() {
+		// Determine how many rows to have with three columns
+		var rows = Math.ceil(this.props.descriptors.length / 3);
+		var desc = Array.from(Array(rows).keys()).map((r) => {
+			var columns = this.props.descriptors.sort().slice(r * 3, (r+1)*3).map((d) => (
+				<div key={d} className="one-third column">
+					<label><input type="checkbox" /><span className="label-body">{d}</span></label>
+				</div>
+			));
+			return <div key={r} className="row">{columns}</div>
+		});
+
 		return (
 			<div>
+				<div>{desc}</div>
 				<TernaryPlot />
 				<Slider />
 			</div>
