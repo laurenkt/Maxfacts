c69882c7c49aa2b04328a02f309dad5ee9867f4b
Lauren <lt696@york.ac.uk>
2016-08-17 12:05:29 +0100

    Fix edit URI

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Content = require('../models/content.js');
-const hbs = require('hbs');
-const sanitizeHtml = require('sanitize-html');
+var Content = require("../models/content.js");
+const hbs = require("hbs");
+const sanitizeHtml = require("sanitize-html");
 
-hbs.registerHelper('shift_headers', function (offset, text) {
-	var headers = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+hbs.registerHelper("shift_headers", function (offset, text) {
+	var headers = ["h1", "h2", "h3", "h4", "h5", "h6"];
 	var transform = {};
 
 	for (var i = 0; i < 6; i++) {
-		// Don't go off the edge of the array
+		// Don"t go off the edge of the array
 		transform[headers[i]] = headers[Math.min(i + offset, 5)];
 	}
 
@@ -18,27 +18,27 @@ hbs.registerHelper('shift_headers', function (offset, text) {
 	return sanitizeHtml(text, {
 		allowedTags:       false,
 		allowedAttributes: false,
-		transformTags:     transform
+		transformTags:     transform,
 	});
 });
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res) => {
 	Promise.all([
-		Content.findFromParentURI('diagnosis').exec(),
-		Content.findFromParentURI('treatment').exec(),
-		Content.findFromParentURI('help').exec(),
+		Content.findFromParentURI("diagnosis").exec(),
+		Content.findFromParentURI("treatment").exec(),
+		Content.findFromParentURI("help").exec(),
 	])
 	.then(([diagnosis, treatment, help]) => {
-		res.render('index', {diagnosis, treatment, help, layout:'layout-fill'});
+		res.render("index", {diagnosis, treatment, help, layout:"layout-fill"});
 	});
 });
 
 // Directory handler
-router.get('/:uri(*)', (req, res, next) => {	
+router.get("/:uri(*)", (req, res, next) => {	
 	Content.findOne( { uri: req.params.uri } )
 		.then(content => {
-			if (!content || content.type != 'directory')
+			if (!content || content.type != "directory")
 				return next();
 			
 			Promise
@@ -47,37 +47,37 @@ router.get('/:uri(*)', (req, res, next) => {
 					// Get links from all parent stages
 						.map(uri => Content
 							.findFromAdjacentURI(uri)
-							.select('title type uri')
+							.select("title type uri")
 							.exec()
 						)
 					// Get siblings of the current page
-						.concat([Content.findFromAdjacentURI(content.uri).select('title type uri').exec()])
+						.concat([Content.findFromAdjacentURI(content.uri).select("title type uri").exec()])
 					// Get children of the current page
-						.concat([Content.findFromParentURI(content.uri).select('title type uri').exec()])
+						.concat([Content.findFromParentURI(content.uri).select("title type uri").exec()])
 					// Also get breadcrumbs
-						.concat([Content.findFromURIs(content.lineage).select('title uri').sort('uri').exec()])
+						.concat([Content.findFromURIs(content.lineage).select("title uri").sort("uri").exec()])
 				)
 				.then(directory => {
 					content.breadcrumbs = directory.pop(); // The breadcrumb lineage will be the last item
 
-					// Don't display children if there is content
-					if (content.body && content.body != '')
+					// Don"t display children if there is content
+					if (content.body && content.body != "")
 						directory.pop();
 
 					content.directory = directory;
 
 					// Provide a way for the template to lookup whether a URI is selected
 					content.selected = {};
-					content.lineage.forEach(uri => content.selected[uri] = 'selected');
+					content.lineage.forEach(uri => content.selected[uri] = "selected");
 					// The current page too
-					content.selected[content.uri] = 'selected';
+					content.selected[content.uri] = "selected";
 
 					// Level count
 					content.levels = directory.length;
 					// Editor URI
-					content.edit_uri = '/editor/' + content.uri;
+					content.edit_uri = "/editor/" + content.uri;
 
-					res.render('list', content);
+					res.render("list", content);
 				})
 				.catch(console.error.bind(console));
 
@@ -85,7 +85,7 @@ router.get('/:uri(*)', (req, res, next) => {
 });
 
 // Page handler
-router.get('/:uri(*)', (req, res, next) => {
+router.get("/:uri(*)", (req, res, next) => {
 	Content.findOne( { uri: req.params.uri } )
 		.then(content => {
 			if (!content)
@@ -95,16 +95,16 @@ router.get('/:uri(*)', (req, res, next) => {
 				return Promise.all([
 					content.getInvalidLinks(),
 					Content.findFromURIs(content.lineage)
-						.select('title uri')
-						.sort('uri')
-						.exec()
+						.select("title uri")
+						.sort("uri")
+						.exec(),
 				])
 				.then(([uris, breadcrumbs]) => {
 					content.invalid_uris = uris;
 					content.breadcrumbs  = breadcrumbs;
 					// Editor URI
-					content.edit_uri = '/editor/' + content.uri;
-					res.render('content', content);
+					content.edit_uri = "/dashboard/directory/" + content.uri;
+					res.render("content", content);
 				});
 		})
 		.catch(console.error.bind(console));
