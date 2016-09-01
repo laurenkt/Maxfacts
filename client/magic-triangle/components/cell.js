716b59df1116580cda5981714017ae99ddcca201
Lauren <lt696@york.ac.uk>
2016-09-01 13:14:49 +0100

    Trying to make the code for the magic triangle more stateless, not 100% sure if that actually is making it clearer

--- client/magic-triangle/components/cell.js
+++ client/magic-triangle/components/cell.js
@@ -2,70 +2,70 @@ import React          from "react";
 import TernaryPlot    from "./ternary-plot";
 import Slider         from "./slider";
 import DescriptorList from "./descriptor-list";
-import {keys}         from "lodash";
 
-export default class Cell extends React.Component {
-	constructor(props) {
-		super(props);
+const ComponentWithDefaultSelected = component => props => {
+	// If there is no selection, and there are exactly three descriptors to choose from
+	if (!props.selected.length && Object.keys(props.context).length === 3)
+		return component({...props, selected: Object.keys(props.context)});
 
-		this.descriptors = keys(props.context);
-	}
+	// Otherwise invoke the component without modifying props
+	return component(props);
+};
 
-	static get propTypes() {
-		return {
-			title:         React.PropTypes.string,
-			context:       React.PropTypes.object.isRequired,
-			selected:      React.PropTypes.arrayOf(React.PropTypes.string),
-			ratios:        React.PropTypes.arrayOf(React.PropTypes.number),
-			severity:      React.PropTypes.number,
-			onChange:      React.PropTypes.func.isRequired,
-			onLabelClick:  React.PropTypes.func.isRequired,
-			onRemoveClick: React.PropTypes.func.isRequired,
-		};
-	}
+const StatelessCell = props => {
+	const plot_label = name => {
+		if (!props.context[name])
+			return name;
+		else
+			return <a href="#" onClick={e => props.onLabelClick(e, name)}>{name}</a>;
+	};
 
-	render() {
-		const derived_step = () =>
-			typeof this.props.severity === "undefined" && typeof this.props.ratios === "undefined" ? 0 : 1;
+	const derived_step = (severity, ratios) =>
+		typeof severity === "undefined" && typeof ratios === "undefined" ? 0 : 1;
 
-		const plot_label = name => {
-			if (!this.props.context[name])
-				return name;
-			else
-				return <a href="#" onClick={e => this.props.onLabelClick(e, name)}>{name}</a>;
-		};
+	const button_label = num_selected =>
+		num_selected === 3 ? "Next" : `Choose ${3 - num_selected} more`;
 
-		var { selected } = this.props;
+	return (
+		<div className="mt-cell">
+			<h3>
+				{props.title || String.fromCodePoint(0x00A0)}
+				{props.title && <a href="#" onClick={props.onRemoveClick}>&#x2715;</a>}
+			</h3>
+			{derived_step(props.severity, props.ratios) === 0 &&
+				<div>
+					<p>Pick <strong>three</strong> categories to compare.</p>
+					<DescriptorList items={Object.keys(props.context)}
+						selected={props.selected}
+						onSelection={selected => props.onChange({selected})} />
+					<button disabled={props.selected.length != 3}
+						onClick={_ => props.onChange({severity: 0.5, ratios:[0.3333, 0.3333, 0.3333], selected:props.selected})}>
+						{button_label(props.selected.length)}
+					</button>
+				</div>}
+			{derived_step(props.severity, props.ratios) === 1 &&
+				<div>
+					<TernaryPlot values={props.ratios} labels={props.selected.map(plot_label)}
+						onChange={ratios => props.onChange({ratios})} />
+					<Slider value={props.severity}
+						onChange={severity => props.onChange({severity})} />
+				</div>}
+		</div>
+	);
+};
 
-		if (!selected.length && this.descriptors.length === 3)
-			selected = this.descriptors;
+StatelessCell.PropTypes = {
+	title:         React.PropTypes.string,
+	context:       React.PropTypes.object.isRequired,
+	selected:      React.PropTypes.arrayOf(React.PropTypes.string),
+	ratios:        React.PropTypes.arrayOf(React.PropTypes.number),
+	severity:      React.PropTypes.number,
+	onChange:      React.PropTypes.func.isRequired,
+	onLabelClick:  React.PropTypes.func.isRequired,
+	onRemoveClick: React.PropTypes.func.isRequired,
+};
 
-		const button_label = (num_remaining = 3 - selected.length) =>
-			num_remaining === 0 ? "Next" : `Choose ${num_remaining} more`;
+const Cell = ComponentWithDefaultSelected(StatelessCell);
+Cell.PropTypes = StatelessCell.PropTypes;
 
-		return (
-			<div className="mt-cell">
-				<h3>
-					{this.props.title || String.fromCodePoint(0x00A0)}
-					{this.props.title && <a href="#" onClick={this.props.onRemoveClick}>&#x2715;</a>}
-				</h3>
-				{derived_step() === 0 &&
-					<div>
-						<p>Pick <strong>three</strong> categories to compare.</p>
-						<DescriptorList items={this.descriptors}
-							selected={selected}
-							onSelection={sel => this.props.onChange({sel})} />
-						<button disabled={selected.length != 3}
-							onClick={_ => this.props.onChange({severity: 0.5, ratios:[0.3333, 0.3333, 0.3333], selected:selected})}>{button_label()}</button>
-					</div>}
-				{derived_step() === 1 &&
-					<div>
-						<TernaryPlot values={this.props.ratios} labels={this.props.selected.map(plot_label)}
-							onChange={ratios => this.props.onChange({ratios})} />
-						<Slider value={this.props.severity}
-							onChange={severity => this.props.onChange({severity})} />
-					</div>}
-			</div>
-		);
-	}
-}
+export default Cell;
