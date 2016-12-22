377af5566eb912e894c78aec3e955af11b0b1543
Lauren <lt696@york.ac.uk>
2016-12-22 16:46:09 +0000

    Auth should be more working now

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,13 +3,16 @@ import { ensureLoggedIn } from "connect-ensure-login";
 
 const router = express.Router();
 
+router.get(/\/.*/i, ensureLoggedIn("/auth"), (req, res, next) => {
+	res.locals.user = req.user;
+	next();
+});
+
 router.use("/users",     require("./dashboard/users.js"));
 router.use("/images",    require("./dashboard/images.js"));
 router.use("/directory", require("./dashboard/directory.js"));
 
 router.get("/", (req, res) => {
-	ensureLoggedIn("/dashboard/auth");
-	console.log(req.user);
 	res.render("dashboard/overview", {layout: "dashboard"});
 });
 
