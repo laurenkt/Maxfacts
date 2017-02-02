bde9ba3c68907fa1b0cec128a1416bc27ce9451b
Lauren <lt696@york.ac.uk>
2017-02-02 11:08:02 +0000

    Removed passwords from users

--- routes/dashboard/users.js
+++ routes/dashboard/users.js
@@ -5,16 +5,16 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
 	User.find().exec().then((users) =>
-		res.render("dashboard/users", {users, layout: "layout-dashboard"}));
+		res.render("dashboard/users", {users, layout: "dashboard"}));
 });
 
 router.get("/new", (req, res) => {
-	res.render("dashboard/user", {layout: "layout-dashboard"});
+	res.render("dashboard/user", {layout: "dashboard"});
 });
 
 router.get("/edit/:id(*)", (req, res) => {
 	User.findOne( { _id: req.params.id } ).then(user => {
-		res.render("dashboard/user", {email:user.email, can_edit_users:user.can_edit_users, layout:"layout-dashboard"});
+		res.render("dashboard/user", {email:user.email, can_edit_users:user.can_edit_users, layout:"dashboard"});
 	});
 });
 
@@ -22,29 +22,17 @@ router.post("/edit/:id(*)", (req, res) => {
 	User.findOne( { _id: req.params.id } ).then(user => {
 		user.email = req.body.email;
 		user.can_edit_users = req.body.can_edit_users;
-
-		if (req.body.password)
-			return user.setHashFromPassword(req.body.password)
-				.then(user.save);
-		else
-			return user.save();
+		return user.save();
 	})
 	.then(() => res.redirect("/dashboard/users"))
 	.catch(console.error.bind(console));
 });
 
 router.post("/new", (req, res) => {
-	var user = new User(req.body);
-
-	if (req.body.password) {
-		user.setHashFromPassword(req.body.password)
-			.then(user.save)
-			.then(() => res.redirect("/dashboard/users"))
-			.catch(console.error.bind(console));
-	}
-	else {
-		throw new Error("No password provided");
-	}
+	const user = new User(req.body);
+	user.save()
+		.then(() => res.redirect("/dashboard/users"))
+		.catch(console.error.bind(console));
 });
 
 module.exports = router;
