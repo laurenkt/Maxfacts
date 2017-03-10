9e718dd9d8ca6c87303dcb2c5114b3bf92cc791d
Lauren <lt696@york.ac.uk>
2017-03-10 16:05:49 +0000

    Command to restore db, used for CI

--- test/Content.js
+++ test/Content.js
@@ -1,22 +1,22 @@
-import {expect} from "chai";
-import Content  from "../models/content.js";
+import {expect} from "chai"
+import Content  from "../models/content.js"
 
 describe("Content", () => {
 	it("should be invalid if there is no URI", done => {
-		const c = new Content();
+		const c = new Content()
 		c.validate(err => {
-			expect(err.errors.uri).to.exist;
-			done();
-		});
-	});
+			expect(err.errors.uri).to.exist
+			done()
+		})
+	})
 
 	it("should be invalid if there is no title", done => {
-		const c = new Content();
+		const c = new Content()
 		c.validate(err => {
-			expect(err.errors.title).to.exist;
-			done();
-		});
-	});
+			expect(err.errors.title).to.exist
+			done()
+		})
+	})
 
 	describe(".getLinksInHTML", () => {
 		it("should get all links in a block of HTML", () => {
@@ -31,34 +31,34 @@ describe("Content", () => {
 				<div>
 					<a title="g" href="h" id="i" />
 				</div>
-			`;
+			`
 
-			const result = Content.getLinksInHTML(html);
+			const result = Content.getLinksInHTML(html)
 			const links = [
 				"1",
 				"D",
 				"e",
 				"h"
-			];
+			]
 
-			expect(result).to.have.members(links);
-		});
+			expect(result).to.have.members(links)
+		})
 
 		it("should only get unique links", () => {
 			const html = `
 				<a href="/1">A</a>
 				<a href="/1">A</a>
 				<a href="1">A</a>
-			`;
+			`
 
-			const result = Content.getLinksInHTML(html);
+			const result = Content.getLinksInHTML(html)
 			const links = [
 				"1",
-			];
+			]
 
-			expect(result).to.have.members(links);
-		});
-	});
+			expect(result).to.have.members(links)
+		})
+	})
 
 	describe(".getImgSrcsInHTML", () => {
 		it("should get all image src's in a block of HTML", () => {
@@ -70,61 +70,61 @@ describe("Content", () => {
 				<figure>
 					<img src="d">
 				</figure>
-			`;
+			`
 
-			const result = Content.getImgSrcsInHTML(html);
+			const result = Content.getImgSrcsInHTML(html)
 			const srcs   = [
 				"a",
 				"b",
 				"c",
 				"d"
-			];
+			]
 
-			expect(result).to.have.members(srcs);
-		});
+			expect(result).to.have.members(srcs)
+		})
 
 		it("should only get unique image src's", () => {
 			const html = `
 				<img src="a" />
 				<img src="/a" />
 				<img src="a" />
-			`;
+			`
 
-			const result = Content.getImgSrcsInHTML(html);
+			const result = Content.getImgSrcsInHTML(html)
 			const srcs   = [
 				"a",
-			];
+			]
 
-			expect(result).to.have.members(srcs);
-		});
-	});
+			expect(result).to.have.members(srcs)
+		})
+	})
 
 	describe(".parentUriFragment", () => {
 		it("should be able to determine the parent URI fragment", () => {
-			const uut = Content.parentUriFragment;
+			const uut = Content.parentUriFragment
 
-			expect( uut("a/b/c") ).to.equal("a/b");
-			expect( uut("a/b")   ).to.equal("a");
-			expect( uut("a/b/c") ).to.equal("a/b");
-			expect( uut("a")     ).to.equal("");
-		});
-	});
+			expect( uut("a/b/c") ).to.equal("a/b")
+			expect( uut("a/b")   ).to.equal("a")
+			expect( uut("a/b/c") ).to.equal("a/b")
+			expect( uut("a")     ).to.equal("")
+		})
+	})
 
 	describe(".normalizeURI", () => {
 		it("should normalize URIs correctly", () => {
-			const uut = Content.normalizeURI;
+			const uut = Content.normalizeURI
 
-			expect (uut("/a/b/c"))    .to.equal("a/b/c");
-			expect (uut("a/B/c"))     .to.equal("a/b/c");
-			expect (uut("a//b//c"))   .to.equal("a/b/c");
-			expect (uut("a!//b$(/c")) .to.equal("a/b/c");
-			expect (uut("a/b/c d/"))  .to.equal("a/b/c-d");
-			expect (uut("a"))         .to.equal("a");
-			expect (uut("/a"))        .to.equal("a");
-			expect (uut("////a"))     .to.equal("a");
-			expect (uut("// / // a")) .to.equal("a");
-			expect (uut(" a "))       .to.equal("a");
-			expect (uut("a b c"))     .to.equal("a-b-c");
-		});
-	});
-});
+			expect (uut("/a/b/c"))    .to.equal("a/b/c")
+			expect (uut("a/B/c"))     .to.equal("a/b/c")
+			expect (uut("a//b//c"))   .to.equal("a/b/c")
+			expect (uut("a!//b$(/c")) .to.equal("a/b/c")
+			expect (uut("a/b/c d/"))  .to.equal("a/b/c-d")
+			expect (uut("a"))         .to.equal("a")
+			expect (uut("/a"))        .to.equal("a")
+			expect (uut("////a"))     .to.equal("a")
+			expect (uut("// / // a")) .to.equal("a")
+			expect (uut(" a "))       .to.equal("a")
+			expect (uut("a b c"))     .to.equal("a-b-c")
+		})
+	})
+})
