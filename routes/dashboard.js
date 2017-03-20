611d0a56d9a13020f7cdf8566d7dc5a759048fb8
Lauren <lt696@york.ac.uk>
2017-03-20 14:35:44 +0000

    Refactored dashboard homepage route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,29 +1,30 @@
-import express from "express";
-import { ensureLoggedIn } from "connect-ensure-login";
-import User from "../models/user.js";
+import express from "express"
+import { ensureLoggedIn } from "connect-ensure-login"
+import User from "../models/user.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.get(/\/.*/i, ensureLoggedIn("/auth"), (req, res, next) => {
-	User.doesUserExist(req.user)
-		.then(valid => {
-			if (valid) {
-				res.locals.user = req.user;
-				next();
-			}
-			else {
-				res.status(403);
-				res.render("dashboard/forbidden", {email:req.user, layout: "dashboard"});
-			}
-		});
-});
+router.get(/\/.*/i, ensureLoggedIn("/auth"), async (req, res, next) => {
+	const is_user_valid = await User.doesUserExist(req.user)
+	
+	if (is_user_valid) {
+		res.locals.user = req.user
+		next()
+	}
+	else {
+		res.status(403)
+		res.render("dashboard/forbidden", {email:req.user, layout: "dashboard"})
+	}
+})
 
-router.use("/users",     require("./dashboard/users.js"));
-router.use("/images",    require("./dashboard/images.js"));
-router.use("/directory", require("./dashboard/directory.js"));
+router.use("/users",     require("./dashboard/users.js"))
+router.use("/images",    require("./dashboard/images.js"))
+router.use("/directory", require("./dashboard/directory.js"))
 
-router.get("/", (req, res) => {
-	res.render("dashboard/overview", {layout: "dashboard"});
-});
+router.get("/", requestOverview)
 
-module.exports = router;
+function requestOverview(req, res) {
+	res.render("dashboard/overview", {layout: "dashboard"})
+}
+
+module.exports = router
