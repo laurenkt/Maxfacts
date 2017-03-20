1064a8125f87e1da9ec0c193656a6ea8a8952544
Lauren <lt696@york.ac.uk>
2017-03-20 14:10:35 +0000

    More coverage for Image

--- models/image.js
+++ models/image.js
@@ -1,54 +1,53 @@
-const mongoose     = require("mongoose");
+const mongoose     = require("mongoose")
+
+const mimetypes = {
+	"image/jpeg":      ".jpg",
+	"image/png":       ".png",
+	"application/pdf": ".pdf",
+}
 
 const ImageSchema = new mongoose.Schema({
 	uri:          {type: String, unique: true, minlength:1, required:true},
 	originalname: {type: String},
 	buffer:       {type: Buffer},
 	encoding:     {type: String},
-	mimetype:     {type: String},
+	mimetype:     {type: String, validate: str => mimetypes.hasOwnProperty(str)},
 	size:         {type: Number},
 }, {
 	timestamps: true,
-});
+})
 
 ImageSchema.statics = {
 	findFromURIs(uris) {
 		return this
 			.find()
-			.where("uri").in(uris);
+			.where("uri").in(uris)
 	},
-};
+
+	normalizeURI(uri, type) {
+		return uri
+			// All lowercase
+			.toLowerCase()
+			// Remove the last fragment after a period
+			.replace(/(.*)(\..*)/g, "$1")
+			// Convert spaces and underscores to dashes (and multiple dashes)
+			.replace(/[_ -]+/g, "-")
+			// Remove any duplicate slashes
+			.replace(/[\/]+/g, "/")
+			// Remove any leading or trailing slashes or dashes
+			.replace(/(^[\/-]+|[\/-]+$)/g, "")
+			// Remove any remaining characters that don"t conform to the URL
+			.replace(/[^a-z0-9-\/]+/g, "")
+			// Append file extension
+			.concat(mimetypes[type])
+	},
+}
 
 ImageSchema.pre("save", function(next) {
-	const mimetypes = {
-		"image/jpeg": ".jpg",
-		"image/png":  ".png",
-		"application/pdf": ".pdf",
-	};
-
-	// Validation
-	if (!mimetypes.hasOwnProperty(this.mimetype)) {
-		next(new Error(`Forbidden mime-type: ${this.mimetype}`));
-	}
-
 	// Force the URI into acceptable format:
-	this.uri = this.uri
-		// All lowercase
-		.toLowerCase()
-		// Remove the last fragment after a period
-		.replace(/(.*)(\..*)/g, "$1")
-		// Convert spaces and underscores to dashes (and multiple dashes)
-		.replace(/[_ -]+/g, "-")
-		// Remove any duplicate slashes
-		.replace(/[\/]+/g, "/")
-		// Remove any leading or trailing slashes or dashes
-		.replace(/(^[\/-]+|[\/-]+$)/g, "")
-		// Remove any remaining characters that don"t conform to the URL
-		.replace(/[^a-z0-9-\/]+/g, "")
-		// Append file extension
-		.concat(mimetypes[this.mimetype]);
+	this.uri = this.model("Image").normalizeURI(this.uri, this.mimetype)
 
-	next();
-});
+	next()
+})
 
-module.exports = mongoose.model("Image", ImageSchema);
+module.exports = mongoose.model("Image", ImageSchema)
--- test/Image.js
+++ test/Image.js
@@ -1,12 +1,66 @@
-import {expect} from "chai";
-import Image  from "../models/image.js";
+import {expect} from "chai"
+import Image  from "../models/image.js"
 
 describe("Image", () => {
 	it("should be invalid if there is no URI", done => {
-		const i = new Image();
+		const i = new Image()
 		i.validate(err => {
-			expect(err.errors.uri).to.exist;
-			done();
-		});
-	});
-});
+			expect(err.errors.uri).to.exist
+			done()
+		})
+	})
+
+	it("should be invalid if MIME type is forbidden", done => {
+		const i = new Image()
+		i.mimetype = "application/javascript"
+		i.validate(err => {
+			expect(err.errors.mimetype).to.exist
+			done()
+		})
+	})
+
+	it("should allow JPEG MIME type", done => {
+		const i = new Image()
+		i.mimetype = "image/jpeg"
+		i.validate(err => {
+			expect(err.errors.mimetype).to.not.exist
+			done()
+		})
+	})
+
+	it("should allow PNG MIME type", done => {
+		const i = new Image()
+		i.mimetype = "image/png"
+		i.validate(err => {
+			expect(err.errors.mimetype).to.not.exist
+			done()
+		})
+	})
+
+	it("should allow PDF MIME type", done => {
+		const i = new Image()
+		i.mimetype = "application/pdf"
+		i.validate(err => {
+			expect(err.errors.mimetype).to.not.exist
+			done()
+		})
+	})
+
+	describe("::normalizeURI", () => {
+		it("should replace extension with correct one", () => {
+			const uri = "test.jpg"
+			const mimetype = "image/png"
+			const expected = "test.png"
+
+			expect(Image.normalizeURI(uri, mimetype)).to.equal(expected)
+		})
+
+		it("should add the correct extension when one is not provided", () => {
+			const uri = "test"
+			const mimetype = "image/jpeg"
+			const expected = "test.jpg"
+
+			expect(Image.normalizeURI(uri, mimetype)).to.equal(expected)
+		})
+	})
+})
