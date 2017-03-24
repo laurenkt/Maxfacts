9ccdad33ed1a04889386666ddb5e1ae583e0acaa
Lauren <lt696@york.ac.uk>
2017-03-24 14:37:42 +0000

    Update features and code-health for Editor
    - `app.js` now follows new project linting rules
    - Some keyboard shortcuts are now supported, e.g. Mod+B for bold,
      Mod(+Shift)+1 for heading-1

--- client/editor/app.js
+++ client/editor/app.js
@@ -1,31 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Editor as RichTextEditor, Plain} from "slate";
-import {serialize, deserialize} from "./serializer.js";
-import Schema from "./schema.js";
+import React    from "react"
+import ReactDOM from "react-dom"
+import Schema   from "./schema.js"
+import {Editor as RichTextEditor} from "slate"
+import {serialize, deserialize}   from "./serializer.js"
 
 class Editor extends React.Component {
 	constructor(props) {
-		super(props);
+		super(props)
 
 		const initial_state = props.value != "" ?
 			deserialize(props.value) :
-			deserialize("<p>Enter content here</p>");
+			deserialize("<p>Enter content here</p>")
 
 		this.state = {
 			slate_state:     initial_state,
 			html_value:      serialize(initial_state),
 			editing_in_html: false,
-		};
+		}
 
-		this.onTextAreaChange = this.onTextAreaChange.bind(this);
-		this.onEditorChange   = this.onEditorChange.bind(this);
-		this.onDocumentChange = this.onDocumentChange.bind(this);
-		this.onEditModeChange = this.onEditModeChange.bind(this);
+		this.onTextAreaChange = this.onTextAreaChange.bind(this)
+		this.onEditorChange   = this.onEditorChange.bind(this)
+		this.onDocumentChange = this.onDocumentChange.bind(this)
+		this.onEditModeChange = this.onEditModeChange.bind(this)
+		this.onKeyDown        = this.onKeyDown.bind(this)
 
-		this.rows = Math.max(props.value.split(/\r?\n/).length, 20);
+		this.rows = Math.max(props.value.split(/\r?\n/).length, 20)
 
-		document.addEventListener("scroll", this.onScroll.bind(this));
+		document.addEventListener("scroll", this.onScroll.bind(this))
 	}
 
 	static get propTypes() {
@@ -34,46 +35,64 @@ class Editor extends React.Component {
 			state:    React.PropTypes.object,
 			value:    React.PropTypes.string,
 			id:       React.PropTypes.string,
-		};
+			name:     React.PropTypes.string,
+			valid_uris: React.PropTypes.array,
+		}
 	}
 
-	onScroll(e) {
+	/**
+	 * Keyboard shortcuts
+	 */
+	onKeyDown(e, data, _) {
+		if (!data.isMod) return
+
+		switch (data.key) {
+			case 'b': return this.onClickMark(e, 'bold')
+			case 'i': return this.onClickMark(e, 'emphasis')
+			case '1': return this.onClickBlock(e, 'heading-1')
+			case '2': return this.onClickBlock(e, 'heading-2')
+			case '3': return this.onClickBlock(e, 'heading-3')
+			default: return
+		}
+	}
+
+	onScroll(_) {
 		// Only do this when the toolbar exists
 		if (this.toolbar) {
 			// And when its parent is above the viewport (so it would be too)
-			const {top} = this.toolbar.parentNode.getBoundingClientRect();
+			const {top} = this.toolbar.parentNode.getBoundingClientRect()
 			if (top < 0) {
-				const width = this.toolbar.parentNode.clientWidth;
-				const offset_y = window.scrollY - this.toolbar.offsetParent.offsetTop;
+				const width = this.toolbar.parentNode.clientWidth
+				const offset_y = window.scrollY - this.toolbar.offsetParent.offsetTop
 				
-				this.toolbar.style.position = "absolute";
-				this.toolbar.style.top      = `${offset_y}px`;
-				this.toolbar.style.width    = `${width}px`;
+				this.toolbar.style.position = "absolute"
+				this.toolbar.style.top      = `${offset_y}px`
+				this.toolbar.style.width    = `${width}px`
 			}
 			else {
-				this.toolbar.style.position = "inherit";
+				this.toolbar.style.position = "inherit"
 			}
 		}
 	}
 
 	onEditorChange(state) {
-		this.setState({slate_state: state});
+		this.setState({slate_state: state})
 	}
 
 	onTextAreaChange(e) {
-		this.rows = Math.min(e.target.value.split(/\r?\n/).length, 20);
-		this.setState({html_value: e.target.value});
+		this.rows = Math.min(e.target.value.split(/\r?\n/).length, 20)
+		this.setState({html_value: e.target.value})
 	}
 
 	onDocumentChange(document, state) {
-		this.setState({html_value: serialize(state)});
+		this.setState({html_value: serialize(state)})
 	}
 
 	onEditModeChange(e) {
 		this.setState({
 			editing_in_html: e.target.checked,
 			slate_state: e.target.checked ? this.state.slate_state : deserialize(this.state.html_value),
-		});
+		})
 	}
 
 	/**
@@ -87,22 +106,20 @@ class Editor extends React.Component {
 		// TODO: convert pasted word doc into our doc
 		// will need URL converter
 		// <span color='red'>...</span> is links links
-		if (data.type != "html") return;
-		if (data.isShift) return;
+		if (data.type != "html") return
+		if (data.isShift) return
 		
-		const { document } = deserialize(data.html);
-
-		console.log(data.html);
+		const { document } = deserialize(data.html)
 
 		return state
 			.transform()
 			.insertFragment(document)
-			.apply();
+			.apply()
 	}
 
 	hasMark(type) {
-		const state = this.state.slate_state;
-		return state.marks.some(mark => mark.type == type);
+		const state = this.state.slate_state
+		return state.marks.some(mark => mark.type == type)
 	}
 
 	/**
@@ -112,8 +129,8 @@ class Editor extends React.Component {
 	 */
 
 	hasInline(type) {
-		const state = this.state.slate_state;
-		return state.inlines.some(inline => inline.type == type);
+		const state = this.state.slate_state
+		return state.inlines.some(inline => inline.type == type)
 	}
 
 	/**
@@ -123,8 +140,8 @@ class Editor extends React.Component {
 	 * @return {Boolean}
 	 */
 	hasBlock(type) {
-		const state = this.state.slate_state;
-		return state.blocks.some(node => node.type == type);
+		const state = this.state.slate_state
+		return state.blocks.some(node => node.type == type)
 	}
 
 	/**
@@ -135,7 +152,7 @@ class Editor extends React.Component {
 	 */
 	onClickMark(e, type) {
 		e.preventDefault()
-		let state = this.state.slate_state;
+		let state = this.state.slate_state
 
 		state = state
 			.transform()
@@ -151,7 +168,7 @@ class Editor extends React.Component {
 	onClickInline(e, type, is_editing) {
 		e.preventDefault()
 
-		let state = this.state.slate_state;
+		let state = this.state.slate_state
 
 		if (type == 'link') {
 			const hasLinks = this.hasInline('link')
@@ -164,8 +181,8 @@ class Editor extends React.Component {
 						.apply()
 				}
 				else {
-					const this_link = state.inlines.find(node => node.type == 'link');
-					const suggested_url = this_link.get("data").get("href");
+					const this_link = state.inlines.find(node => node.type == 'link')
+					const suggested_url = this_link.get("data").get("href")
 					const href = window.prompt('Enter the URL of the link:', suggested_url)
 					if (href) {
 						state = state
@@ -178,17 +195,16 @@ class Editor extends React.Component {
 
 			else if (state.isExpanded) {
 				// Find what text the user has selected
-				const current_selection = state.document.selection;
-				const selected_text = state.document.getDescendant(state.focusKey).text.slice(state.startOffset, state.endOffset);
+				const selected_text = state.document.getDescendant(state.focusKey).text.slice(state.startOffset, state.endOffset)
 
 				// Extract any url in a [...], filter out preceding or trailing whitespace
-				const matched_url = selected_text.match(/[ ]*\[(.*?)\][ ]*$/);
-				let suggested_url = '';
+				const matched_url = selected_text.match(/[ ]*\[(.*?)\][ ]*$/)
+				let suggested_url = ''
 
 				if (matched_url && matched_url.length >= 2)
-					suggested_url = matched_url[1]; // First match
+					suggested_url = matched_url[1] // First match
 
-				const chars_to_remove = matched_url ? matched_url[0].length : 0;
+				const chars_to_remove = matched_url ? matched_url[0].length : 0
 
 				const href = window.prompt('Enter the URL of the link:', suggested_url)
 
@@ -209,7 +225,7 @@ class Editor extends React.Component {
 						})
 						.wrapInline({
 							type: 'link',
-							data: { href }
+							data: { href },
 						})
 						.collapseToEnd()
 						.apply()
@@ -229,7 +245,7 @@ class Editor extends React.Component {
 							.extendBackward(text.length)
 							.wrapInline({
 								type: 'link',
-								data: { href }
+								data: { href },
 							})
 							.collapseToEnd()
 							.apply()
@@ -249,27 +265,23 @@ class Editor extends React.Component {
 	 */
 
 	onClickBlock(e, type) {
-		const DEFAULT_NODE = "paragraph";
+		const DEFAULT_NODE = "paragraph"
 
 		e.preventDefault()
-		let state = this.state.slate_state;
+		let state = this.state.slate_state
 		let transform = state.transform()
 		const { document } = state
 		
 		// aside must nest paragraphs rather than replace them
 		if (type == 'aside') {
-			const isActive = this.hasBlock(type)
-			const isType = state.blocks.some((block) => {
+			const isType = state.blocks.some(block => {
 				return !!document.getClosest(block.key, parent => parent.type == type)
 			})
 
-			if (isType) {
-				transform
-					.unwrapBlock(type);
-			}
-			else {
-				transform.wrapBlock(type);
-			}
+			if (isType)
+				transform.unwrapBlock(type)
+			else
+				transform.wrapBlock(type)
 		}
 
 		else
@@ -351,7 +363,7 @@ class Editor extends React.Component {
 
 	renderInlineButton(type, icon) {
 		const isActive = this.hasInline(type)
-		const is_editing = icon == 'edit';
+		const is_editing = icon == 'edit'
 		const onMouseDown = e => this.onClickInline(e, type, is_editing)
 
 		return (
@@ -405,25 +417,26 @@ class Editor extends React.Component {
 						state={this.state.slate_state}
 						onChange={this.onEditorChange}
 						onDocumentChange={this.onDocumentChange}
-						onPaste={this.onPaste} />}
+						onPaste={this.onPaste}
+						onKeyDown={this.onKeyDown} />}
 			</div>
-		);
+		)
 	}
 }
 
-document.addEventListener("DOMContentLoaded", e => {
-	let valid_uris = JSON.parse(document.getElementById("editor_data").innerHTML);
-	valid_uris = valid_uris.uris ? valid_uris.uris : [];
+document.addEventListener("DOMContentLoaded", _ => {
+	let valid_uris = JSON.parse(document.getElementById("editor_data").innerHTML)
+	valid_uris = valid_uris.uris ? valid_uris.uris : []
 
-	const textarea  = document.getElementsByTagName("textarea").item(0);
-	const container = document.createElement("div");
-	container.className = "content-editor";
+	const textarea  = document.getElementsByTagName("textarea").item(0)
+	const container = document.createElement("div")
+	container.className = "content-editor"
 
 	ReactDOM.render(<Editor 
 		name={textarea.getAttribute("name")}
 		value={textarea.value}
 		valid_uris={valid_uris}
-	/>, container);
+	/>, container)
 	
-	textarea.parentNode.replaceChild(container.childNodes[0], textarea);
-});
+	textarea.parentNode.replaceChild(container.childNodes[0], textarea)
+})
