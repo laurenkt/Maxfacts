690d8752f4d60c678c3916d877d9fd24804fc1c8
Lauren <lt696@york.ac.uk>
2017-06-20 15:34:03 +0100

    Improve search results page

--- routes/search.js
+++ routes/search.js
@@ -1,25 +1,48 @@
 import express from 'express';
 import Content from '../models/content';
+import {escapeRegExp} from "lodash"
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', getSearchResults)
+
+async function getSearchResults(req, res, next) {
+	try {
 	if (req.query.query) {
-		Content
+		res.locals.query = req.query.query
+
+		const results = await Content
 			.find(
 				{ $text : { $search : req.query.query } }, 
 				{ score : { $meta: "textScore" } }
 			)
+			.where('hide', false)
 			.sort({ score : { $meta : 'textScore' } })
 			.exec()
-			.then(results => {
-				res.render('search', {results});
-			})
-			.catch(console.error.bind(console));
+
+		let regexp = new RegExp('^([\\s\\S]*)(' + escapeRegExp(req.query.query) + ')([\\s\\S]*)$', 'i')
+
+		for (let i = 0; i < results.length; i++) {
+			let match = results[i].getMatchedParagraph(regexp)
+			if (match !== null) {
+				results[i].match = {
+					before: match[1].length < 100 ? match[1] : match[1].replace(/^[\s\S]*?\b([\s\S]{0,100})*$/i, '...$1'),
+					match: match[2],
+					after: match[3].length < 100 ? match[3] : match[3].replace(/^([\s\S]{0,100})\b[\s\S]*$/i, '$1...'),
+				}
+			}
+			results[i].breadcrumbs = await results[i].getBreadcrumbs()
+		}
+
+		res.render('search', {results});
 	}
 	else {
 		res.render("search");
 	}
-});
+	}
+	catch (e) {
+		console.error(e)
+	}
+}
 
 module.exports = router;
