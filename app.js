7a3a676a55e37d0c1d019bb79e9c6ef28c23761e
Lauren <lt696@york.ac.uk>
2017-09-11 09:54:43 +0100

    Fix code style changes

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-import express    from "express"
-import session    from "express-session"
-import morgan     from "morgan"
-import chalk      from "chalk"
-import bodyParser from "body-parser"
-import mongoose   from "mongoose"
-import mongoStore from "connect-mongo"
-import passport   from "passport"
-import {Strategy} from "passport-google-oauth20"
-import {join}     from "path"
-import fs         from "fs"
-import hbs        from "express-handlebars"
-import joi        from "joi"
-import {config}   from "dotenv"
+import express    from 'express'
+import session    from 'express-session'
+import morgan     from 'morgan'
+import chalk      from 'chalk'
+import bodyParser from 'body-parser'
+import mongoose   from 'mongoose'
+import mongoStore from 'connect-mongo'
+import passport   from 'passport'
+import {Strategy} from 'passport-google-oauth20'
+import {join}     from 'path'
+import hbs        from 'express-handlebars'
+import joi        from 'joi'
+import {config}   from 'dotenv'
 
 // Load and validate environment variables
 config() // loads from .env file
@@ -29,7 +28,7 @@ const env_schema = joi.object({
 
 const {error} = joi.validate(process.env, env_schema)
 if (error) {
-	throw new Error("Config validation error: " + error.message)
+	throw new Error('Config validation error: ' + error.message)
 }
 // Set-up Mongoose models
 mongoose.Promise = global.Promise // Required to squash a deprecation warning
@@ -37,17 +36,17 @@ async function connect() {
 	await mongoose.connect(`mongodb://${process.env.MONGO_URI}`, {useMongoClient:true})
 }
 
-connect().catch(console.error.bind(console, "connection error:"))
+connect().catch(console.error.bind(console, 'connection error:'))
 
 // Start Express
 const app = express()
 
 // Views in templates/ using handlebars.js
-app.engine("hbs", hbs({
-	extname:       ".hbs",
-	defaultLayout: "main",
-	layoutsDir:    join(__dirname, "templates", "layouts"),
-	partialsDir:   join(__dirname, "templates", "partials"),
+app.engine('hbs', hbs({
+	extname:       '.hbs',
+	defaultLayout: 'main',
+	layoutsDir:    join(__dirname, 'templates', 'layouts'),
+	partialsDir:   join(__dirname, 'templates', 'partials'),
 	helpers: {
 		toJSON: obj => JSON.stringify(obj),
 		date:   date => date.toLocaleString('en-GB', {
@@ -56,19 +55,19 @@ app.engine("hbs", hbs({
 			month: 'long',
 			day: 'numeric',
 			timeZone: 'UTC',
-			timeZoneName: 'short'
+			timeZoneName: 'short',
 		}).slice(0, -5), // remove time-zone
 	},
 }))
-app.set("view engine", "hbs")
-app.set("views", join(__dirname, "templates"))
-app.use((req, res, next) => { res.locals.path = req.path; next(); }) // make path available to templates
+app.set('view engine', 'hbs')
+app.set('views', join(__dirname, 'templates'))
+app.use((req, res, next) => { res.locals.path = req.path; next() }) // make path available to templates
 
 // Logging in the console
 // Don't log in test runner
 if (!process.env.TEST) {
-	morgan.token("time", () => `${(new Date()).getHours()}:${(new Date()).getMinutes()}`)
-	morgan.token("cstatus", (req, res) => {
+	morgan.token('time', () => `${(new Date()).getHours()}:${(new Date()).getMinutes()}`)
+	morgan.token('cstatus', (req, res) => {
 		const code = res.statusCode
 		const colorer = code >= 500 ? chalk.white
 			: code >= 400 ? chalk.yellow
@@ -78,8 +77,8 @@ if (!process.env.TEST) {
 
 		return colorer(code)
 	})
-	app.use(morgan(`\u2753 ${chalk.yellow("Request:")} :method ${chalk.inverse(":url")} (:time)`, {immediate:true}))
-	app.use(morgan(`\u2755 ${chalk.green("Response:")} :method ${chalk.inverse(":url")} :cstatus :response-time ms - :res[content-length]`))
+	app.use(morgan(`\u2753 ${chalk.yellow('Request:')} :method ${chalk.inverse(':url')} (:time)`, {immediate:true}))
+	app.use(morgan(`\u2755 ${chalk.green('Response:')} :method ${chalk.inverse(':url')} :cstatus :response-time ms - :res[content-length]`))
 }
 
 // Process POST request bodies
@@ -87,7 +86,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // Sessions
 app.use(session({
-	secret: "f8e1a0f9-e7a9-4e0d-8a2e-0624a3f62510", // Just a generated UUID
+	secret: 'f8e1a0f9-e7a9-4e0d-8a2e-0624a3f62510', // Just a generated UUID
 	saveUninitialized: false,
 	resave: false,
 	store: new (mongoStore(session))({mongooseConnection: mongoose.connection}), // Store session data in mongodb
@@ -104,7 +103,7 @@ passport.use(new Strategy({
 		cb(null, email)
 	}
 	else {
-		cb({error: "Invalid email"}, null)
+		cb({error: 'Invalid email'}, null)
 	}
 }
 ))
@@ -116,29 +115,29 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // Middleware for static files into static/
-app.use(express.static(join(__dirname, "static")))
+app.use(express.static(join(__dirname, 'static')))
 
 // Loads the named module from the routes/ directory
-const route = (name) => require(join(__dirname, "routes", name))
+const route = (name) => require(join(__dirname, 'routes', name))
 
-app.use("/",                   route("feedback"))
-app.use("/",                   route("images"))
-app.use("/",                   route("videos"))
+app.use('/',                   route('feedback'))
+app.use('/',                   route('images'))
+app.use('/',                   route('videos'))
 app.use(
-	"/help/oral-food/recipes", route("recipes"))
-app.use("/magic-triangle",     route("magic_triangle"))
-app.use("/dashboard",          route("dashboard"))
-app.use("/search",             route("search"))
-app.use("/map.xml",            route("map"))
-app.use("/",                   route("index"))
+	'/help/oral-food/recipes', route('recipes'))
+app.use('/magic-triangle',     route('magic_triangle'))
+app.use('/dashboard',          route('dashboard'))
+app.use('/search',             route('search'))
+app.use('/map.xml',            route('map'))
+app.use('/',                   route('index'))
 
-app.get("/auth", passport.authenticate("google", { scope: ["email"] } ))
-app.get("/auth/callback",
-	passport.authenticate("google", { successReturnToOrRedirect: "/", failureRedirect: "/error" }))
+app.get('/auth', passport.authenticate('google', { scope: ['email'] } ))
+app.get('/auth/callback',
+	passport.authenticate('google', { successReturnToOrRedirect: '/', failureRedirect: '/error' }))
 
 // If nothing is found
 app.use((req, res, next) => {
-	const err = new Error("Not Found")
+	const err = new Error('Not Found')
 	err.status = 404
 	next(err) // pass to error handler
 })
@@ -148,7 +147,7 @@ app.use((err, req, res, _) => {
 	let errInfo = {}
 
 	// Show more information if development
-	if (app.get("env") === "development") {
+	if (app.get('env') === 'development') {
 		// Don't bother logging a stack trace for 404 errors
 		if (err.status != 404)
 			console.error(err.stack)
@@ -157,7 +156,7 @@ app.use((err, req, res, _) => {
 	}
 
 	res.status(err.status || 500)
-	res.render("error", {
+	res.render('error', {
 		message: err.message,
 		error: errInfo,
 	})
